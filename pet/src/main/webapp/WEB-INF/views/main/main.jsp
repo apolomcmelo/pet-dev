b2b4639eedd2e6d9c462f60f67a5ae722c9b114b
reinaldo.lindoso <reinaldo.lindoso@acd3cb73-42cb-f2a9-cf45-0d56e487e4b2>
2014-05-13 23:17:44 +0000

    Adicionado busca de CEP via AJAX para popular demais campos do endereço

--- pet/src/main/webapp/WEB-INF/views/main/main.jsp
+++ pet/src/main/webapp/WEB-INF/views/main/main.jsp
@@ -4,5 +4,8 @@
 <link rel="stylesheet" type="text/css" href='${pageContext.servletContext.contextPath}/res/css/bootstrap.min.css' />
 <link rel="stylesheet" type="text/css" href='${pageContext.servletContext.contextPath}/res/css/bootstrap-theme.min.css' />
 <link rel="stylesheet" type="text/css" href='${pageContext.servletContext.contextPath}/res/css/util.css' />
-<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery-1.3.2.js" ></script>
-<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery.maskedinput-1.1.4.js" ></script>
+<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery-1.10.2.js" ></script>
+<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery-1.8.3.min.js" ></script>
+<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery.maskedinput.min.js" ></script>
+<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/jquery.maskedinput.js" ></script>
+<script type="text/javascript" src="${pageContext.servletContext.contextPath}/res/js/util.js" ></script>
---
+++ pet/src/main/webapp/res/js/util.js
@@ -0,0 +1,58 @@
+// Mascara dos campos
+jQuery(function($){
+		$("#birth").mask("99/99/9999");
+		$("#phone").mask("(99) 9999-9999");
+		$("#cellphone").mask("(99) 99999-9999");
+		$("#zipcode").mask("99999-999");
+	});
+
+//Responsável por realizar a busca do CEP digitado e popular os demais campos de endereço
+$(function(){
+
+	    /**
+	     * Atribuo ao elemento #zipcode, o evento blur
+	     * Blur, dispara uma ação, quando o foco
+	     * sair do elemento, no nosso caso cep
+	     */
+	    $("#zipcode").blur(function(){
+	        /**
+	         * Resgatamos o valor do campo #cep
+	         * usamos o replace, pra remover tudo que não for numérico,
+	         * com uma expressão regular
+	         */
+	        var cep     = $(this).val().replace(/[^0-9]/, '');
+	        //Armazena a referência da div#boxCampos
+	        var boxes   = $("#boxCampos");
+	         //Armazena a referência da div#mensagemErro
+	        var msgErro = $("#mensagemErro");
+	  
+	        //Verifica se não está vazio
+	        if(cep !== ""){
+	             //Cria variável com a URL da consulta, passando o CEP
+	             var url = 'http://cep.correiocontrol.com.br/'+cep+'.json';
+	           
+	             /**
+	              * Fazemos um requisição a URL, como vamos retornar json,
+	              * usamos o método $.getJSON;
+	              * Que é composta pela URL, e a função que vai retorna os dados
+	              * o qual passamos a variável json, para recuperar os valores
+	              */
+	             $.getJSON(url, function(json){
+	                    //Atribuimos o valor aos inputs
+	                    $("#street").val(json.logradouro);
+	                    $("#city").val(json.localidade);
+	                    $("#state").val(json.uf);
+	                    /**
+	                     * Removemos a classe ocultar, para mostrar os campos
+	                     * preenchidos
+	                     */
+	                    boxes.removeClass('ocultar');
+	                 
+	                    //Usamos o método fail, caso não retorne nada
+	                }).fail(function(){
+	                 //Não retornando um valor válido, ele mostra a mensagem
+	                 msgErro.removeClass('ocultar').html('CEP inexistente')
+	            });
+	        }
+	    });
+	});
