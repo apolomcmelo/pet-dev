8772f0158b87187c49b449c8b6d4db2db1c8758e
reinaldo.lindoso <reinaldo.lindoso@acd3cb73-42cb-f2a9-cf45-0d56e487e4b2>
2014-08-26 18:23:52 +0000

    Correção do NGO e PetShop para cadastrar senha em hash, gravar photo e validar se usuario ja existe em base antes do cadastro para não ocorrer erro de PK.

--- pet/src/main/java/br/com/usjt/tcc/controller/NgoController.java
+++ pet/src/main/java/br/com/usjt/tcc/controller/NgoController.java
@@ -1,12 +1,21 @@
 package br.com.usjt.tcc.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import br.com.usjt.tcc.helper.HashHelper;
 import br.com.usjt.tcc.interfaces.dao.NGODao;
+import br.com.usjt.tcc.interfaces.dao.UserDao;
 import br.com.usjt.tcc.model.NGO;
 import br.com.usjt.tcc.model.RegisterNest;
+import br.com.usjt.tcc.model.User;
 
 @Controller
 public class NgoController {
@@ -14,17 +23,41 @@ public class NgoController {
 	@Autowired
 	private NGODao ngoDao;
 	
+	@Autowired
+	UserDao userDao;
+	
 	@RequestMapping("registerNGO")
-	public String registerNGO(RegisterNest registerNest) {
-				
-		NGO ngo = new NGO();
-		ngo.setName(registerNest.getCompany().getName());
-		ngo.setCnpj(registerNest.getCompany().getCnpj());
-		ngo.setSite(registerNest.getCompany().getSite());
-		ngo.setAdministrator(registerNest.getUser());
-		ngoDao.adiciona(ngo);
+	public String registerNGO(RegisterNest registerNest, HttpServletRequest request) {
 		
-		return "/user/login";
+		String retorno = null;
+		NGO ngo = new NGO();
+		User user = registerNest.getUser();
+		try {
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			MultipartFile file =  multipartRequest.getFile("file");
+			user.setFoto(file.getBytes());
+			user.setPassword(HashHelper.sha256(user.getPassword()));
+			user.setCellphone(user.getCellphone().replaceAll("\\D", ""));  
+			user.setPhone(user.getPhone().replaceAll("\\D", ""));
+			ngo.setName(registerNest.getCompany().getName());
+			ngo.setCnpj(registerNest.getCompany().getCnpj());
+			ngo.setSite(registerNest.getCompany().getSite());
+			user.setIsOfNGO(true);
+			ngo.setAdministrator(registerNest.getUser());
+			
+			if(userDao.buscaUserEmail(user.getEmail())){
+				request.setAttribute("existeUser", true);
+				retorno = "user/register";
+			}else{
+				ngoDao.adiciona(ngo);
+				retorno = "user/login";
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return retorno;
 	}
 	
 }
--- pet/src/main/java/br/com/usjt/tcc/controller/PetShopController.java
+++ pet/src/main/java/br/com/usjt/tcc/controller/PetShopController.java
@@ -1,12 +1,21 @@
 package br.com.usjt.tcc.controller;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import br.com.usjt.tcc.helper.HashHelper;
 import br.com.usjt.tcc.interfaces.dao.PetShopDao;
+import br.com.usjt.tcc.interfaces.dao.UserDao;
 import br.com.usjt.tcc.model.PetShop;
 import br.com.usjt.tcc.model.RegisterNest;
+import br.com.usjt.tcc.model.User;
 
 @Controller
 public class PetShopController {
@@ -14,18 +23,42 @@ public class PetShopController {
 	@Autowired
 	private PetShopDao petShopDao;
 
+	@Autowired
+	UserDao userDao;
+
 	@RequestMapping("registerPetShop")
-	public String registerPetShop(RegisterNest registerNest) {
+	public String registerPetShop(RegisterNest registerNest, HttpServletRequest request) {
 
+		String retorno = "";
 		PetShop petShop = new PetShop();
-		petShop.setName(registerNest.getCompany().getName());
-		petShop.setCnpj(registerNest.getCompany().getCnpj());
-		petShop.setSite(registerNest.getCompany().getSite());
-		petShop.setAdministrator(registerNest.getUser());
-
-		petShopDao.adiciona(petShop);
-
-		return "/user/login";
+		User user = registerNest.getUser();
+		
+		try {
+			MultipartHttpServletRequest multipartRequest = (MultipartHttpServletRequest) request;
+			MultipartFile file = multipartRequest.getFile("file");
+			user.setFoto(file.getBytes());
+			user.setPassword(HashHelper.sha256(user.getPassword()));
+			user.setCellphone(user.getCellphone().replaceAll("\\D", ""));  
+			user.setPhone(user.getPhone().replaceAll("\\D", ""));
+			user.setIsOfPetShop(true);
+			
+			petShop.setName(registerNest.getCompany().getName());
+			petShop.setCnpj(registerNest.getCompany().getCnpj());
+			petShop.setSite(registerNest.getCompany().getSite());
+			petShop.setAdministrator(registerNest.getUser());
+			
+			if(userDao.buscaUserEmail(user.getEmail())){
+				request.setAttribute("existeUser", true);
+				retorno = "user/register";
+			}else{
+				petShopDao.adiciona(petShop);
+				retorno = "user/login";
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return retorno;
 	}
 
 	@RequestMapping("novoProduto")
